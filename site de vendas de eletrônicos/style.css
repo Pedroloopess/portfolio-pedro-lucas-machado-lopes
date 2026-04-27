3c3d00189b04349ef5d91dbf3ce40559e8305ef0
Pedroloopess <pedrolucasp503@gmail.com>
2026-04-27 20:47:02 -0300

    Create style.css

---
+++ site de vendas de eletrônicos/style.css
@@ -0,0 +1,14 @@
+const menuToggle = document.getElementById("menuToggle");
+const navbar = document.getElementById("navbar");
+const mensagemCarrinho = document.getElementById("mensagemCarrinho");
+
+let total = 0;
+
+menuToggle.addEventListener("click", () => {
+  navbar.classList.toggle("ativo");
+});
+
+function adicionarCarrinho(produto) {
+  total++;
+  mensagemCarrinho.textContent = `${produto} adicionado. Total: ${total}`;
+}
